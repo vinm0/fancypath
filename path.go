2717ab8443b160afd2534a7f0f36ca395fb30fe9
Vincent Martinez <vincentem20@gmail.com>
2021-08-22 16:59:22 -0500

    Removed braces, Added comments

--- path.go
+++ path.go
@@ -8,32 +8,58 @@ import (
 )
 
 type path struct {
+	// Pattern values parsed from pattern string
+	//
 	pattern []string
+
+	// Path values parsed from http request
+	//
 	request []string
-	vars    map[string]string
-	query   url.Values
-	frag    string
+
+	// Mapped subpaths using pattern subpaths as key
+	// and http request subpaths as value.
+	//
+	// Values are retrieved by calling the Var(key string) method
+	//
+	vars map[string]string
+
+	// Query values obtained from the http request.
+	//
+	// Values are retrieved by calling the Query(key string) method.
+	//
+	query url.Values
+
+	// Frag contains the path fragment value
+	//
+	// Example: "https://example.com/blog/article/#42"
+	//
+	//		p := NewPath(r, "")
+	//		p.Frag == "42"  // true
+	//
+	Frag string
 }
 
 // NewPath parses the request url against the given pattern.
-// Returns a pointer to the parsed path object.
 //
 // Pattern: The pattern always begins with the root slash.
-// Identify variable subpaths within the pattern by
-// enclosing them in curly braces, "{varName}". All other
-// subpaths are ignored. Each subpath may contain only one
+// Identify variable subpaths within the pattern by providing
+// a term in the subpath. Each subpath may contain only one
 // variable and the variable must span the entire subpath.
+// To ignore a subpath, substitute a subpath with an astrisk, "*".
 //
 // Example: "https://example.com/blog/random/first-post/"
 //
 //		// Pattern always begins from the root slash
-//		pattern := "/anything/{category}/{slug}/{id}"
+//		pattern := "/*/category/slug/id"
 //		p := NewPath(r, pattern)
 //
-//		p.Var("category") ==> "random"
-//		p.var("slug") ==> "first-post"
-//		p.var("id") ==> ""
+//		p.Var("category") == "random" // true
+//		p.Var("slug") == "first-post" // true
+//		p.Var("id") == "" // true
+//		p.Var("*") == "" // true
+//
 func Newpath(r *http.Request, pattern string) *path {
+	// Must have root slash
 	if !strings.HasPrefix(pattern, "/") {
 		pattern = "/" + pattern
 	}
@@ -41,24 +67,25 @@ func Newpath(r *http.Request, pattern string) *path {
 	p := path{}
 	p.parsePattern(pattern)
 	p.parseReq(r.URL.Path)
-
-	p.vars = map[string]string{}
 	p.mapVars()
 
 	p.query = r.URL.Query()
-	p.frag = r.URL.Fragment
+	p.Frag = r.URL.Fragment
 
 	return &p
 }
 
 // Var returns the value corresponding to the path variable
 //
-// Example:
-//		"https://example.com/edit/article/42"
-// 		Pattern: "/anything/{category}/{id}"
+// Example: "https://example.com/edit/article/42"
 //
-//		p.Var("category") ==> "article"
-//		p.Var("id")	==> "42"
+// 		pattern = "/*/category/id"
+//		p := NewPath(r, pattern)
+//
+//		p.Var("category") == "article" // true
+//		p.Var("id")	== "42" // true
+//		p.Var("*") == "" // true
+//
 func (p *path) Var(key string) (val string) {
 	return p.vars[key]
 }
@@ -68,6 +95,7 @@ func (p *path) Var(key string) (val string) {
 //
 // val returns 0 if the corresponding value cannot
 // be converted to an int or does not exist.
+//
 func (p *path) VarInt(key string) (val int, ok bool) {
 	val, err := strconv.Atoi(p.vars[key])
 	return val, (err == nil)
@@ -78,67 +106,48 @@ func (p *path) VarInt(key string) (val int, ok bool) {
 // Example:
 // 		"https://example.com/edit/article/?item=42"
 //
-//		p.Query("item")	==> "42"
+//		p.Query("item") == "42" // true
+//
 func (p *path) Query(key string) string {
 	return p.query.Get(key)
 }
 
-// Frag returns the path fragment value
-//
-// Example:
-//		"https://example.com/blog/article/#42"
-//
-//		p.Frag() ==> "42"
-func (p *path) Frag() string {
-	return p.frag
-}
-
 // parseReq loads the request field with the string slice
 // representation of the request url path.
+//
 func (p *path) parseReq(path string) {
 	parse(&p.request, path)
 }
 
 // parsePattern loads the pattern field with the string slice
 // representation of the given pattern
+//
 func (p *path) parsePattern(pattern string) {
 	parse(&p.pattern, pattern)
 }
 
 // mapVars identifies variables in the defined pattern and maps
 // the variable to the corresponding value in the http request.
+//
 func (p *path) mapVars() {
+	p.vars = map[string]string{}
+
 	for i, v := range p.pattern {
 		if i >= len(p.request) {
 			break
 		}
 
-		if isVar(v, true) { // v == "{var}"
-			k := strings.TrimSpace(v[1 : len(v)-1])
-			p.vars[k] = p.request[i]
-		}
-
+		k := strings.TrimSpace(v)
+		p.vars[k] = p.request[i]
 	}
+
+	delete(p.vars, "*")
 }
 
 // parse loads the slice with slash separated substrings.
 // parse is a helper function used to load the path slice fields
+//
 func parse(s *[]string, path string) {
 	// Exclude first element, always empty
 	*s = strings.Split(path, "/")[1:]
-}
-
-// isVar returns true if val is a string formatted as a variable
-// (i.e., "{var}"). Otherwise, returns false.
-//
-// Set strict to true if val must be validated as a variable.
-// Set strict to false if val only needs to be recognized as a variable.
-func isVar(val string, strict bool) bool {
-	if !strict {
-		return strings.HasPrefix(val, "{")
-	}
-
-	return strings.HasPrefix(val, "{") &&
-		strings.HasSuffix(val, "}") &&
-		strings.Index(val[1:], "{") == -1
 }
